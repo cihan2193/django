295029bd957135ceb854aef9ffe731edf63c964d
unknown <you@example.com>
2019-06-06 14:04:12 +0300

    first commit

---
+++ user/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
+# Create your views here.
+def register(request):
+    """form=RegisterForm(request.POST or None)
+    if form.is_valid():
+        username=form.cleaned_data.get("username")
+        password=form.cleaned_data.get("password")
+        newUser=User(username=username)
+        newUser.set_password(password)
+        newUser.save()
+        login(request,newUser)
+        messages.info(request,"Basariyla Kayit oldunnuz")
+        return redirect("index")
+    context={
+            "form":form
+        }  
+    return render(request,"register",context)"""
+    #ikinci secenek
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password")
+            newUser=User(username=username)
+            newUser.set_password(password)
+            newUser.save()
+            login(request,newUser)
+            messages.info(request,"Basariyla Kayit oldunnuz")
+            return redirect("index")
+        context={
+            "form":form
+        }  
+        return render(request,"register.html",context)
+    else:
+        form=RegisterForm()
+        context={
+            "form":form
+        }  
+        return render(request,"register.html",context)
+def loginUser(request):
+    form=LoginForm(request.POST or None)
+    context={
+        "form":form
+    }
+    if form.is_valid():
+        username=form.cleaned_data.get("username")
+        password=form.cleaned_data.get("password")
+        user=authenticate(username=username,password=password)
+        if user is None:
+            messages.info(request,"Kullanici adi veya parola hatali")
+            return render(request,"login.html",context)
+        messages.success(request,"Basariyla giris yaptin")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
+def logoutUser(request):
+    logout(request)
+    messages.success(request,"Basariyla cikis yaptiniz")
+    return redirect("index")
